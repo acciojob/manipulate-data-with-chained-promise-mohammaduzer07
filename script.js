51a50d434860fa3b602b55077060d3487056e099
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-05-26 15:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+let ouput = document.getElementById('output');
+let arr = [1,2,3,4];
+function promise(arr, time) {
+	return new Promise((resolve){
+		setTimeout(() =>{
+			resolve(arr);
+		}, time);
+	});
+}
+
+promise(arr, 3000);
+.then((res) =>{
+	const even = res.filter((num) =>{
+		return num % 2 === 0;
+	});
+	return promise(even, 1000);
+})
+.then((res) =>{
+	ouput.innerText = res;
+	const multi = res.map((num) =>{
+		return num * 2;
+	});
+	return promise(multi, 2000);
+})
+.then((res)=>{
+	ouput.innerText = res;
+})
+.catch((error)=>{
+	console.log(error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
